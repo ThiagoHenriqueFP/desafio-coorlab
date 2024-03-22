fefda911973e0dc84e8b74229bcf16bb023d5bb0
Thiago Henrique <thiagohp070@gmail.com>
2024-03-22 20:27:14 -0300

    fix: backend script

--- app/back.sh
+++ app/back.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ../
-python3 -m uvicorn app.backend.main:app --reload --port 3000
+python3 -m uvicorn app.backend.main:app --reload --port 3000 || python -m uvicorn app.backend.main:app --reload --port 3000
